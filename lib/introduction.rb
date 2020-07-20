4856a2f966b8cbd85e22bc27d2b91cfef1492887
Brodrick George <brodrick.george@yahoo.com>
2020-07-20 21:37:21 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,11 +6,7 @@ introduction 'Josh'
 
 
 
-def introduction_with_language ("Dan", "Ember.js")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
-end
 
-introduction_with_language
 
 
 
